6d55ae8b0d28bad71239b22c75761f96d534551c
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-08 14:51:01 -0400

    Add T.SelectFollow(Func<T,T?>)

---
+++ source/TylerDM.StandardLibrary.Tests/System/Linq/LinqExtTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TylerDM.StandardLibrary.System.Linq;
+
+public static class LinqExtTests
+{
+	class TestSelectFollowClass() { public TestSelectFollowClass? Next { get; init; } }
+
+	[Fact]
+	public static void TestSelectFollow()
+	{
+		var chain = new TestSelectFollowClass() { Next = new() { Next = new() } };
+		var array = chain.SelectFollow(x => x.Next).ToArray();
+		if (array.Length != 3)
+			throw new Exception("Incorrect number of elements returned.");
+	}
+}
---
+++ source/TylerDM.StandardLibrary/System/Linq/LinqExt.cs
@@ -0,0 +1,14 @@
+﻿namespace TylerDM.StandardLibrary.System.Linq;
+
+public static class LinqExt
+{
+	public static IEnumerable<T> SelectFollow<T>(this T start, Func<T, T?> getNext)
+	{
+		var next = start;
+		while (next is not null)
+		{
+			yield return next;
+			next = getNext(next);
+		}
+	}
+}
