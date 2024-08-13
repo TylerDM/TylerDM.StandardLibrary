13cb89bd67378d8de9ff9a7f37df6e2f13a8f299
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-13 17:16:24 -0400

    Add SelectWithSurrounding<(T?,T?,T?)(this IEnumerable<T>).

--- source/TylerDM.StandardLibrary.Tests/System/Linq/LinqExtTests.cs
+++ source/TylerDM.StandardLibrary.Tests/System/Linq/LinqExtTests.cs
@@ -5,7 +5,44 @@ public static class LinqExtTests
 	class TestSelectFollowClass() { public TestSelectFollowClass? Next { get; init; } }
 
 	[Fact]
-	public static void TestSelectFollow()
+	public static void SelectWithSurrounding()
+	{
+		int[] numbers = [1, 2, 3];
+		var enumerator = numbers.SelectWithSurroundingStruct().GetEnumerator();
+		moveAndValidate(true);
+		int? previous, current, next;
+
+		void validate(int? expectedPrev, int? expectedCurrent, int? expectedNext)
+		{
+			if (previous != expectedPrev)
+				throw new Exception("Previous was incorrect.");
+			if (current != expectedCurrent)
+				throw new Exception("Current was incorrect.");
+			if (next != expectedNext)
+				throw new Exception("Next was incorrect.");
+		}
+
+		void moveAndValidate(bool expected)
+		{
+			if (enumerator.MoveNext() != expected)
+				throw new Exception("Incorrect enumerable length");
+		}
+
+		(previous, current, next) = enumerator.Current;
+		moveAndValidate(true);
+		validate(null, 1, 2);
+
+		(previous, current, next) = enumerator.Current;
+		moveAndValidate(true);
+		validate(1, 2, 3);
+
+		(previous, current, next) = enumerator.Current;
+		moveAndValidate(false);
+		validate(2, 3, null);
+	}
+
+	[Fact]
+	public static void SelectFollow()
 	{
 		var chain = new TestSelectFollowClass() { Next = new() { Next = new() } };
 		var array = chain.SelectFollow(x => x.Next).ToArray();
@@ -14,7 +51,7 @@ public static class LinqExtTests
 	}
 
 	[Fact]
-	public static void TestSelectMany()
+	public static void SelectMany()
 	{
 		var array = new int[][] { [0], [0, 0] };
 		var flattened = array.SelectMany().ToArray();
--- source/TylerDM.StandardLibrary/System/Linq/LinqExt.cs
+++ source/TylerDM.StandardLibrary/System/Linq/LinqExt.cs
@@ -2,6 +2,69 @@
 
 public static class LinqExt
 {
+	public static IEnumerable<(T? Previous, T? Current, T? Next)> SelectWithSurrounding<T>(this IEnumerable<T> enumerable)
+		where T : class
+	{
+		T? previous = null;
+		T? current = null;
+		T? next = null;
+
+		(T?, T?, T?) build() => (previous, current, next);
+
+		var first = true;
+		foreach (var item in enumerable)
+		{
+			previous = current;
+			current = next;
+			next = item;
+
+			if (first)
+				first = false;
+			else
+				yield return build();
+		}
+
+		previous = current;
+		current = next;
+		next = default;
+		yield return build();
+	}
+
+	public static IEnumerable<(T? Previous, T? Current, T? Next)> SelectWithSurroundingStruct<T>(this IEnumerable<T> enumerable)
+		where T : struct
+	{
+		T? previous = null;
+		T? current = null;
+		T? next = null;
+
+		(T?, T?, T?) build() => (previous, current, next);
+
+		var first = true;
+		foreach (var item in enumerable)
+		{
+			previous = current;
+			current = next;
+			next = item;
+
+			if (first)
+				first = false;
+			else
+				yield return build();
+		}
+
+		previous = current;
+		current = next;
+		next = default;
+		yield return build();
+	}
+
+	public static IEnumerable<(int Index, T Element)> SelectWithIndex<T>(this T[] array)
+	{
+		var i = 0;
+		foreach (var element in array)
+			yield return (i++, element);
+	}
+
 	public static IEnumerable<T> SelectFollow<T>(this T root, Func<T, T?> getNext, bool includeRoot = true)
 	{
 		var next = includeRoot ? root : getNext(root);
