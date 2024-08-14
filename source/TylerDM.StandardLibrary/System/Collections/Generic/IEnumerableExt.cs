6bf7978b17bdb0ecb3db3b60b765cf2f41227a1a
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-14 13:26:55 -0400

    Add IEnumerable.ForEach<T>(Action<T>).

--- source/TylerDM.StandardLibrary/System/Collections/Generic/IEnumerableExt.cs
+++ source/TylerDM.StandardLibrary/System/Collections/Generic/IEnumerableExt.cs
@@ -1,36 +1,40 @@
-﻿using TylerDM.StandardLibrary.System.Collections.Generic;
-
-namespace TylerDM.StandardLibrary.System.Collections.Generic;
+﻿namespace TylerDM.StandardLibrary.System.Collections.Generic;
 
 public static class IEnumerableExt
 {
-    public static IEnumerable<T> SelectMany<T>(this IEnumerable<IEnumerable<T>> enumerable) =>
-        enumerable.SelectMany(x => x);
+	public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+	{
+		foreach (var item in enumerable)
+			action(item);
+	}
 
-    public static bool None<TSource>(this IEnumerable<TSource> enumerable, Func<TSource, bool> predicate) =>
-        enumerable.Any(predicate) == false;
+	public static IEnumerable<T> SelectMany<T>(this IEnumerable<IEnumerable<T>> enumerable) =>
+			enumerable.SelectMany(x => x);
 
-    public static bool None<TSource>(this IEnumerable<TSource> enumerable) =>
-        enumerable.Any() == false;
+	public static bool None<TSource>(this IEnumerable<TSource> enumerable, Func<TSource, bool> predicate) =>
+			enumerable.Any(predicate) == false;
 
-    public static TSource WithMax<TSource, TKey>(this IEnumerable<TSource> enumerable, Func<TSource, TKey> func) =>
-        enumerable.OrderByDescending(x => func(x)).First();
+	public static bool None<TSource>(this IEnumerable<TSource> enumerable) =>
+			enumerable.Any() == false;
 
-    public static TSource WithMin<TSource, TKey>(this IEnumerable<TSource> enumerable, Func<TSource, TKey> func) =>
-        enumerable.OrderBy(x => func(x)).First();
+	public static TSource WithMax<TSource, TKey>(this IEnumerable<TSource> enumerable, Func<TSource, TKey> func) =>
+			enumerable.OrderByDescending(x => func(x)).First();
 
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable)
-        where T : notnull
-    {
-        foreach (var item in enumerable)
-            if (item is not null)
-                yield return item;
-    }
+	public static TSource WithMin<TSource, TKey>(this IEnumerable<TSource> enumerable, Func<TSource, TKey> func) =>
+			enumerable.OrderBy(x => func(x)).First();
 
-    public static IEnumerable<string> WhereNotNullOrWhitespace<T>(this IEnumerable<string?> enumerable)
-    {
-        foreach (var item in enumerable)
-            if (string.IsNullOrWhiteSpace(item) == false)
-                yield return item;
-    }
+	public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> enumerable)
+			where T : notnull
+	{
+		foreach (var item in enumerable)
+			if (item is not null)
+				yield return item;
+	}
+
+	public static IEnumerable<string> WhereNotNullOrWhitespace<T>(this IEnumerable<string?> enumerable)
+	{
+		foreach (var item in enumerable)
+			if (string.IsNullOrWhiteSpace(item) == false)
+				yield return item;
+	}
 }
--- source/TylerDM.StandardLibrary/Usings.cs
+++ source/TylerDM.StandardLibrary/Usings.cs
@@ -11,3 +11,4 @@ global using System.Linq;
 global using System.Threading.Tasks;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.Threading;
+global using TylerDM.StandardLibrary.System.Collections.Generic;
