599b947697f1d619cfa3c41d83680738b803fec8
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-08-14 17:20:16 -0400

    Add tools to keep you from forgetting SemaphoreSlim.Release(), because I did.

---
+++ source/TylerDM.StandardLibrary/System/Threading/SemaphoreSlimExt.cs
@@ -0,0 +1,82 @@
+﻿namespace TylerDM.StandardLibrary.System.Threading;
+
+public static class SemaphoreSlimExt
+{
+	public static void WaitThen(this SemaphoreSlim semaphore, Action action)
+	{
+		try
+		{
+			semaphore.Wait();
+			action();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+
+	public static T WaitThen<T>(this SemaphoreSlim semaphore, Func<T> action)
+	{
+		try
+		{
+			semaphore.Wait();
+			return action();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+
+	public static async Task WaitThenAsync(this SemaphoreSlim semaphore, Func<Task> funcTask)
+	{
+		try
+		{
+			await semaphore.WaitAsync();
+			await funcTask();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+
+	public static async Task<T> WaitThenAsync<T>(this SemaphoreSlim semaphore, Func<Task<T>> funcTask)
+	{
+		try
+		{
+			await semaphore.WaitAsync();
+			return await funcTask();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+
+	public static async Task WaitThenAsync(this SemaphoreSlim semaphore, Action action)
+	{
+		try
+		{
+			await semaphore.WaitAsync();
+			action();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+
+	public static async Task<T> WaitThenAsync<T>(this SemaphoreSlim semaphore, Func<T> func)
+	{
+		try
+		{
+			await semaphore.WaitAsync();
+			return func();
+		}
+		finally
+		{
+			semaphore.Release();
+		}
+	}
+}
