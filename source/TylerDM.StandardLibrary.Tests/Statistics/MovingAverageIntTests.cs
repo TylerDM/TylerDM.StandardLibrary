15f444c35afc3b5b827fc93867ae3ecba11c1849
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-09-20 12:29:01 -0400

    Add MovingAverageInt.

---
+++ source/TylerDM.StandardLibrary.Tests/Statistics/MovingAverageIntTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TylerDM.StandardLibrary.Statistics;
+
+public class MovingAverageIntTests
+{
+	[Fact]
+	public void Test()
+	{
+		var x = new MovingAverageInt(5);
+		Assert.True(x.Empty);
+		Assert.False(x.Full);
+		Assert.ThrowsAny<Exception>(() => x.GetAverage());
+		Assert.False(x.TryGetAverage(out var _));
+
+		x.Add(1);
+		Assert.False(x.Empty);
+		Assert.False(x.Full);
+		Assert.Equal(1, x.GetAverage());
+		var trySuccess = x.TryGetAverage(out var tryResult);
+		Assert.True(trySuccess);
+		Assert.Equal(1, tryResult);
+
+		x.Add(10);
+		Assert.Equal(5, x.GetAverage());
+	}
+}
---
+++ source/TylerDM.StandardLibrary/Statistics/MovingAverageInt.cs
@@ -0,0 +1,39 @@
+﻿namespace TylerDM.StandardLibrary.Statistics;
+
+public class MovingAverageInt
+{
+	private readonly FixedQueue<int> _queue;
+
+	public int SampleSize => _queue.Capacity;
+	public bool Empty => _queue.Empty;
+	public bool Full => _queue.Full;
+
+	public MovingAverageInt(int sampleSize, int initialValue = default)
+	{
+		ArgumentOutOfRangeException.ThrowIfLessThan(sampleSize, 2);
+
+		_queue = new(sampleSize);
+
+		if (initialValue == default) return;
+		for (int i = 0; i < sampleSize; i++)
+			Add(initialValue);
+	}
+
+	public void Add(int sample) =>
+	_queue.Enqueue(sample);
+
+	public int GetAverage() =>
+		_queue.Sum() / _queue.Count;
+
+	public bool TryGetAverage(out int avg)
+	{
+		if (Empty)
+		{
+			avg = default;
+			return false;
+		}
+
+		avg = GetAverage();
+		return true;
+	}
+}
