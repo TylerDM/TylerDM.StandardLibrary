73872d03f3e79e107d2281c57d215f3e8821bdec
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-09-20 12:40:53 -0400

    Add AutoSizedList/Factory.

---
+++ source/TylerDM.StandardLibrary/Optimization/AutoSizedList.cs
@@ -0,0 +1,14 @@
+﻿namespace TylerDM.StandardLibrary.Optimization;
+
+public class AutoSizedList<T>(AutoSizedListFactory<T> factory, int initialCapacity) : List<T>(initialCapacity), IDisposable
+{
+	private bool disposed = false;
+
+	public void Dispose()
+	{
+		if (disposed) return;
+		disposed = true;
+
+		factory.ReportFinalSize(Count);
+	}
+}
---
+++ source/TylerDM.StandardLibrary/Optimization/AutoSizedListFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace TylerDM.StandardLibrary.Optimization;
+
+public class AutoSizedListFactory<T>(int sampleSize = 10, int initialValue = default, float multiplier = 1.25f)
+{
+	private readonly MovingAverageInt _movingAverage = new(sampleSize, initialValue);
+
+	public int SampleSize => _movingAverage.SampleSize;
+	/// <summary>
+	/// The multiplier by which the average is multiplied by to get the final list capacity.
+	/// </summary>
+	public float Multiplier { get; } = multiplier;
+
+	public AutoSizedList<T> Create() =>
+		new(this, getListSize());
+
+	internal void ReportFinalSize(int finalSize) =>
+		_movingAverage.Add(finalSize);
+
+	private int getListSize() =>
+		Convert.ToInt32(_movingAverage.GetAverage() * Multiplier);
+}
