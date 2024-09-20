7a2f820b91403bceeabdab898dc9d4cf828a9afe
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-09-20 12:22:43 -0400

    Add FixedQueue.

---
+++ source/TylerDM.StandardLibrary.Tests/Collections/FixedQueueTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TylerDM.StandardLibrary.Collections;
+
+public class FixedQueueTests
+{
+	[Fact]
+	public void Test()
+	{
+		const int capacity = 5;
+
+		var q = new FixedQueue<int>(capacity);
+		Assert.Equal(capacity, q.Capacity);
+		Assert.True(q.Empty);
+
+		q.Enqueue(1);
+		Assert.Single(q);
+
+		q.Enqueue(2);
+		q.Enqueue(3);
+		q.Enqueue(4);
+		q.Enqueue(5);
+		Assert.True(q.Full);
+
+		q.Enqueue(6);
+		Assert.DoesNotContain(1, q);
+
+		var dequeued = q.Dequeue();
+		Assert.Equal(2, dequeued);
+		Assert.False(q.Full);
+	}
+}
