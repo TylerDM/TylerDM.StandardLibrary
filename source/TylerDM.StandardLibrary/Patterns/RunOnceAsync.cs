7b34ea814e24caaa386357bcb381d5e0242a166b
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-10-01 14:53:38 -0400

    Add RunOnce/Async.

---
+++ source/TylerDM.StandardLibrary/Patterns/RunOnceAsync.cs
@@ -0,0 +1,14 @@
+﻿namespace TylerDM.StandardLibrary.Patterns;
+
+public class RunOnceAsync(Func<Task> _func)
+{
+	private bool ran = false;
+
+	public async ValueTask RunAsync()
+	{
+		if (ran) return;
+		ran = true;
+
+		await _func();
+	}
+}
