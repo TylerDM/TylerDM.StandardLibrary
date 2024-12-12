2b60feb799e44db2337e4801cfb81613391d5bab
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-12-12 14:45:04 -0500

    Add Ct.ShouldContinue() and TimeSpan.WaitAsync().

---
+++ source/TylerDM.StandardLibrary/System/Threading/CancellationTokenExt.cs
@@ -0,0 +1,7 @@
+﻿namespace TylerDM.StandardLibrary.System.Threading;
+
+public static class CancellationTokenExt
+{
+    public static bool ShouldContinue(this CancellationToken ct) =>
+        ct.IsCancellationRequested == false;
+}
