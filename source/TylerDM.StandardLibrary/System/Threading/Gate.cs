6f76bb14be7a75c282c5ee6a6edfc62e63012216
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-12-20 12:27:36 -0500

    Added Gate.cs

---
+++ source/TylerDM.StandardLibrary/System/Threading/Gate.cs
@@ -0,0 +1,22 @@
+﻿namespace TylerDM.StandardLibrary.System.Threading;
+
+public class Gate()
+{
+	private readonly SemaphoreSlim _semaphore = new(0);
+
+	public async Task<bool> TryWaitAsync(CancellationToken ct)
+	{
+		try
+		{
+			await _semaphore.WaitAsync(ct);
+			return true;
+		}
+		catch (OperationCanceledException)
+		{
+			return false;
+		}
+	}
+
+	public void Release() =>
+		_semaphore.Release(int.MaxValue);
+}
