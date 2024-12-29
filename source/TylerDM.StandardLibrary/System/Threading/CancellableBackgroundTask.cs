c50d19d6e558e1a047c89e479f34b5eafca53e83
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2024-12-29 13:56:00 -0500

    Add CancellableBackgroundTask.

---
+++ source/TylerDM.StandardLibrary/System/Threading/CancellableBackgroundTask.cs
@@ -0,0 +1,42 @@
+﻿namespace TylerDM.StandardLibrary.System.Threading;
+
+/// <summary>
+/// Represents a cancellable task running in the background. Calling Dispose() cancels the task. Use to run methods in the background until a scope ends.
+/// </summary>
+public class CancellableBackgroundTask : IDisposable
+{
+    private readonly CancellationTokenSource _cts = new();
+    private readonly Task _task;
+    
+    private bool disposed = false;
+
+    public CancellableBackgroundTask(Func<CancellationToken, Task> func) =>
+        _task = Task.Run(() => func(_cts.Token));
+    
+    public CancellableBackgroundTask(Action<CancellationToken> action) =>
+        _task = Task.Run(() => action(_cts.Token));
+
+    /// <summary>
+    /// Wait for the task to complete.
+    /// </summary>
+    public Task WaitAsync(CancellationToken ct = default) =>
+        _task.WaitAsync(ct);
+    
+    /// <summary>
+    /// Wait for the task to complete.
+    /// </summary>
+    /// <returns>True if the task completed successfully.</returns>
+    public Task<bool> TryWaitAsync(CancellationToken ct = default) =>
+        _task.TryWaitAsync(ct);
+    
+    /// <summary>
+    /// Cancels the background task and disposes unmanaged internal resources.
+    /// </summary>
+    public void Dispose()
+    {
+        if (disposed) return;
+        disposed = true;
+        
+        _cts.Dispose();
+    }
+}
