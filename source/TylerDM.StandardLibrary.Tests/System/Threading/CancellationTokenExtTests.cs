363a53db17fb72979005e44b8f8f350e8a30ebde
Tyler Mays <16845057+TylerDM@users.noreply.github.com>
2025-01-02 15:50:34 -0500

    Add tests to verify these two classes actually work.

---
+++ source/TylerDM.StandardLibrary.Tests/System/Threading/CancellationTokenExtTests.cs
@@ -0,0 +1,33 @@
+﻿namespace TylerDM.StandardLibrary.System.Threading;
+
+public class CancellationTokenExtTests
+{
+    [Fact]
+    public async Task WaitForCancelAsyncTest()
+    {
+        Task letPropagateAsync() =>
+            TimeSpan.FromMilliseconds(100).WaitAsync(); 
+        
+        using var cts = new CancellationTokenSource();
+        var running = false;
+        
+        void setRunning(bool value) =>
+            running = value;
+        
+        var task = Task.Run(async () =>
+        {
+            setRunning(true);
+            await cts.Token.WaitForCancelAsync();
+            setRunning(false);
+        });
+        
+        await letPropagateAsync();
+        Assert.True(running);
+        Assert.False(task.IsCompleted);
+
+        cts.Cancel();
+        await letPropagateAsync();
+        Assert.False(running);
+        Assert.True(task.IsCompletedSuccessfully);
+    }
+}
